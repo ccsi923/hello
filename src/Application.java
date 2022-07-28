ba204c1d16799e026fcf32104b598439600d3e94
Raul Requena <raulrequec@gmail.com>
2022-07-28 19:10:10 +0200

    Convert eur to USD

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Please enter the amount of euros");
+
+        Scanner sc = new Scanner(System.in);
+        double eur = sc.nextDouble();
+
+        System.out.println("The amount on dollars is " + eur * 1.01);
     }
 }
